41fbe5baf8a394542716edcbbd02a8021c13c31a
Abhiram Manoj <42737673+AbhiramM1@users.noreply.github.com>
2023-11-10 00:17:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function alrt(){
+    alert("Not Connected to the Internet");
+}
+function clicked() {
+    document.getElementById("demo").style.color = "gray";
+  }
+function greyed()
+{
+    document.getElementById('grey').style.backgroundColor="grey";
+}
+function greyed1(){
+    document.getElementById('grey1').style.backgroundColor="grey";
+}
